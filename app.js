98088c73f69a4010e41da2728de69f1430e1f27b
dahidalgoa <danielhidalgo06@gmail.com>
2025-02-14 16:57:14 -0500

    Crear función agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Array de los participantes del juego
+let amigos = [];
+
+function agregarAmigo() {
+    
+    //Capturar el valor del campo de entrada
+    let participante = document.getElementById('amigo');
+    let nombreAmigo = participante.value;
+
+    //Validar la información recolectada
+    if((nombreAmigo === '') || (/^[0-9]+$/.test(nombreAmigo))) {
+            alert('Por favor, ingrese un nombre válido')
+    } else if(amigos.includes(nombreAmigo)) {
+        alert(`El nombre ${nombreAmigo} ya existe en la lista, por favor ingresar otro nombre o el apellido`);
+    } else {
+        //Actualizar array de amigos
+        amigos.push(nombreAmigo);
+    }
+    console.log(amigos);
+
+    //Limpiar el campo de entrada
+    participante.value = '';
+    return;
+    }
